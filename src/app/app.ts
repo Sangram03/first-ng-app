a85f6cf0cf1cd0b9127c485457f7d6e7374dc86f
Sangram Das <sangramraju143@gmail.com>
2026-04-08 18:45:24 +0530

    styles changes app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
+import { HomeComponent } from "./home/home";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   template: `
-    <app-header></app-header>
+    <app-header>
+      <main>
+        <app-home/>
+      </main>
+    </app-header>
 
     <h1>{{ title() }}</h1>
 
     <router-outlet></router-outlet>
-  `,
+  `, styles:[
+    `
+    main {
+      padding: 16px;
+    }`
+  ]
 })
 export class App {
   title = signal('My Angular App');
